236d65e27a652271893ab86fa2863f5ea58cc373
Shaun <Shaunfruggie1257@gmail.com>
2025-12-31 22:40:03 -0500

    mroe ballin????

--- Assets/Scripts/loopingMover.cs
+++ Assets/Scripts/loopingMover.cs
@@ -5,16 +5,21 @@ public class loopingMover : MonoBehaviour
 
     // Core idea here is to apply a constant movement
     public Vector3 offset;
+    public float bonusOffset;
     float accumulator = 0;
     [SerializeField] float cap = 1; 
     [SerializeField] float stepVal;
 
     private void Awake()
     {
-        offset = transform.position;
+        offset = transform.localPosition - Vector3.left * bonusOffset;
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("balls");
+        }
 
         accumulator += Time.deltaTime * stepVal;
         transform.localPosition = accumulator * new Vector3(1,-1,0) - offset;
